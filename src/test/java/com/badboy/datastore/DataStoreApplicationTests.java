2b8c38d43df20357d538d059db5055c80bf3eff7
Sujiv Shrestha <sshrest3@ford.com>
2022-01-22 23:26:54 -0500

    Adding project setup files | first commit

---
+++ src/test/java/com/badboy/datastore/DataStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badboy.datastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
